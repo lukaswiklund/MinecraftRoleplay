f2aafea1638b161aa7a31d27f9676dab5d057098
Lukas Wiklund <lukas@wiklund.se>
2019-06-02 10:25:35 +0200

    Updated formatting

--- src/main/java/se/wiklund/minecraftroleplay/company/CompanyCommand.java
+++ src/main/java/se/wiklund/minecraftroleplay/company/CompanyCommand.java
@@ -29,12 +29,12 @@ import se.wiklund.minecraftroleplay.utils.JsonUtils;
 public class CompanyCommand implements CommandExecutor {
 
 	private static final String[][] COMMAND_DESCRIPTIONS = new String[][] {
-			new String[] { "company list", "List your companies" },
-			new String[] { "company info <Name>", "See company info" },
-			new String[] { "company details <Name>", "See detailed information about your company" },
-			new String[] { "company register <Name>", "Register a new company" },
-			new String[] { "company deregister <Name>", "Deregister a company" },
-		};
+		new String[] { "company list", "List your companies" },
+		new String[] { "company info <Name>", "See company info" },
+		new String[] { "company details <Name>", "See detailed information about your company" },
+		new String[] { "company register <Name>", "Register a new company" },
+		new String[] { "company deregister <Name>", "Deregister a company" },
+	};
 
 	private Main main;
 
